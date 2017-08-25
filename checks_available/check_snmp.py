ee50ce6df026a9e35c1616bfb31c47fc96d2e985
Ara Sadoyan <ara.sadoyan@netangels.net>
2017-08-25 18:52:11 +0400

    SNMP Update

--- checks_available/check_snmp.py
+++ checks_available/check_snmp.py
@@ -3,66 +3,31 @@ import lib.getconfig
 import lib.puylogger
 import datetime
 import os
+import configparser
 from pysnmp.hlapi import *
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
+config = configparser.RawConfigParser()
+config_file = os.getcwd() + '/../conf/snmp.ini'
+config.read(config_file)
+
 check_type = 'snmp'
 cluster_name = lib.getconfig.getparam('SelfConfig', 'cluster_name')
-server = lib.getconfig.getparam('SNMP', 'server')
-port = lib.getconfig.getparam('SNMP', 'port')
-auth_protocol = lib.getconfig.getparam('SNMP', 'authProtocol')
-priv_protocol = lib.getconfig.getparam('SNMP', 'privProtocol')
-AuthPassphrase = lib.getconfig.getparam('SNMP', 'AuthPassphrase')
-PrivPassphrase = lib.getconfig.getparam('SNMP', 'PrivPassphrase')
-SecurityName = lib.getconfig.getparam('SNMP', 'SecurityName')
-auth = lib.getconfig.getparam('SNMP', 'auth')
 
-mibsfile = os.getcwd() + '/../conf/snmpmibs.txt'
-reaction = -3
-
-
-authprotos = {'MD5': usmHMACMD5AuthProtocol,
-              'SHA': usmHMACSHAAuthProtocol}
-protocols = {'DES': usmDESPrivProtocol,
-             '3DES': usm3DESEDEPrivProtocol,
-             'AES128': usmAesCfb128Protocol,
-             'AES192': usmAesCfb192Protocol,
-             'AES256': usmAesCfb256Protocol}
-
-if auth_protocol in authprotos:
-    authProtocol = authprotos[auth_protocol]
-
-if priv_protocol in protocols:
-    privProtocol = protocols[priv_protocol]
-
-
-def nonblank_lines(f):
-    for l in f:
-        line = l.rstrip()
-        if line:
-            yield line
-
-mibo = {}
-
-with open(mibsfile) as f_in:
-    for line in nonblank_lines(f_in):
-        if not line.strip().startswith('#'):
-            (key, val) = line.split()
-            mibo[key] = val
-
-f_in.close()
-
+authprotos = {'MD5': usmHMACMD5AuthProtocol, 'SHA': usmHMACSHAAuthProtocol}
+protocols = {'DES': usmDESPrivProtocol, '3DES': usm3DESEDEPrivProtocol,'AES128': usmAesCfb128Protocol, 'AES192': usmAesCfb192Protocol,'AES256': usmAesCfb256Protocol}
+servantvalues = ['auth' ,'port', 'server', 'authprotocol', 'privprotocol', 'securityname', 'authpassphrase', 'privpassphrase']
 
 def runcheck():
     local_vars = []
 
     timestamp = int(datetime.datetime.now().strftime("%s"))
     try:
-        def getValuesF(n, oid):
+        def getValuesF(n, oid, srv, prt, host):
             cmdGen = cmdgen.CommandGenerator()
 
             errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
-                cmdgen.CommunityData('public', mpModel=0), cmdgen.UdpTransportTarget((server, port)),
+                cmdgen.CommunityData('public', mpModel=0), cmdgen.UdpTransportTarget((srv, prt)),
                 cmdgen.MibVariable(oid),
             )
 
@@ -77,15 +42,15 @@ def runcheck():
                           )
                 else:
                     for name, val in varBinds:
-                        local_vars.append({'name': 'snmp_' + n, 'timestamp': timestamp, 'value': val})
-        def getValuesT(name, oid):
+                        local_vars.append({'name': 'snmp_' + n, 'timestamp': timestamp, 'value': val, 'hostname' : host})
+        def getValuesT(name, oid, srv, prt, host, authProto, privProto, SecName, AuthPass, PrivPass):
             errorIndication, errorStatus, errorIndex, varBinds = next(
                 getCmd(SnmpEngine(),
-                       UsmUserData(SecurityName, AuthPassphrase, PrivPassphrase,
-                                   authProtocol=authProtocol,
-                                   privProtocol=privProtocol
+                       UsmUserData(SecName, AuthPass, PrivPass,
+                                   authProtocol=authProto,
+                                   privProtocol=privProto
                                    ),
-                       UdpTransportTarget((server, port)),
+                       UdpTransportTarget((srv, prt)),
                        ContextData(),
                        ObjectType(ObjectIdentity(oid)))
             )
@@ -97,21 +62,23 @@ def runcheck():
             else:
                 for varBind in varBinds:
                     value = str(varBind.prettyPrint()).replace(' ', '').split('=')[1]
-                    local_vars.append({'name': 'snmp_' + name, 'timestamp': timestamp, 'value': value})
+                    local_vars.append({'name': 'snmp_' + name, 'timestamp': timestamp, 'value': value , 'hostname' : host})
 
-        for k, v in mibo.items():
-            if auth is True:
-                getValuesT(k, v)
+        for t in config.sections():
+            a = dict(config.items(t))
+            if a['auth'] == 'True':
+                for k, v in a.items():
+                    if k not in servantvalues:
+                        ap = authprotos[a['authprotocol']]
+                        pp = protocols[a['privprotocol']]
+                        getValuesT(k, v, a['server'], a['port'], t, ap, pp, a['securityname'], a['authpassphrase'], a['privpassphrase'])
             else:
-                getValuesF(k, v)
+                for k, v in a.items():
+                    if k not in servantvalues:
+                        getValuesF(k, v, a['server'], a['port'], t)
+
         return local_vars
     except Exception as e:
         lib.puylogger.print_message(__name__ + ' Error : ' + str(e))
         pass
-
-
-
-
-
-
 
