c15d6246a733a217546adba6a45e4f2a8ba7a64b
Ara Sadoyan <ara.sadoyan@netangels.net>
2018-02-12 19:13:05 +0400

    Solr bug fix

--- checks_available/check_solr.py
+++ checks_available/check_solr.py
@@ -17,7 +17,7 @@ class Check(lib.basecheck.CheckBase):
     def precheck(self):
         try:
             stats_json = json.loads(lib.commonclient.httpget(__name__, solr_url))
-            requests = ('active', 'delete', 'get', 'head', 'move', 'options', 'other', 'put', 'trace')
+            requests = ('delete', 'get', 'head', 'move', 'options', 'other', 'put', 'trace')
             responses = ('1xx', '2xx', '3xx', '4xx', '5xx')
             heapstats = ('committed', 'init', 'max', 'used')
             sothreads = ('threads.count', 'threads.daemon.count')
@@ -25,38 +25,38 @@ class Check(lib.basecheck.CheckBase):
                    'gc.G1-Old-Generation.count','gc.G1-Old-Generation.time' ,'gc.G1-Young-Generation.count', 'gc.G1-Young-Generation.time')
             for rqst in requests:
                 rqst_name = 'org.eclipse.jetty.server.handler.DefaultHandler.' + rqst + '-requests'
-                rqvalue= stats_json['metrics'][1][rqst_name]['count']
+                rqvalue= stats_json['metrics']['solr.jetty'][rqst_name]['count']
                 csrate = self.rate.record_value_rate('slr_'+rqst, rqvalue, self.timestamp)
                 self.local_vars.append({'name': 'solr_' + rqst + '_requests', 'timestamp': self.timestamp, 'value': csrate, 'check_type': check_type, 'chart_type': 'Rate'})
-    
+
             total_requests = 'org.eclipse.jetty.server.handler.DefaultHandler.requests'
-            trv = stats_json['metrics'][1][total_requests]['count']
+            trv = stats_json['metrics']['solr.jetty'][total_requests]['count']
             rqrate = self.rate.record_value_rate('slr_total_requests', trv, self.timestamp)
             self.local_vars.append({'name': 'solr_requests_all', 'timestamp': self.timestamp, 'value': rqrate, 'check_type': check_type, 'chart_type': 'Rate'})
-    
+
             for resp in responses:
                 resp_name = 'org.eclipse.jetty.server.handler.DefaultHandler.' + resp + '-responses'
-                csvalue = stats_json['metrics'][1][resp_name]['count']
+                csvalue = stats_json['metrics']['solr.jetty'][resp_name]['count']
                 csrate = self.rate.record_value_rate('slr_'+resp, csvalue, self.timestamp)
                 self.local_vars.append({'name': 'solr_' + resp + '_responses', 'timestamp': self.timestamp, 'value': csrate, 'check_type': check_type, 'chart_type': 'Rate'})
-    
+
             for hu in heapstats:
                 hu_name = 'memory.heap.' + hu
-                huvalue = stats_json['metrics'][3][hu_name]['value']
+                huvalue = stats_json['metrics']['solr.jvm'][hu_name]
                 self.local_vars.append({'name': 'solr_heap_' + hu, 'timestamp': self.timestamp, 'value': huvalue, 'check_type': check_type})
-    
+
             for nohu in heapstats:
                 nohu_name = 'memory.non-heap.' + nohu
-                nohuvalue = stats_json['metrics'][3][nohu_name]['value']
-                self.local_vars.append({'name': 'solr_heap_' + nohu, 'timestamp': self.timestamp, 'value': nohuvalue, 'check_type': check_type})
-    
+                nohuvalue = stats_json['metrics']['solr.jvm'][nohu_name]
+                self.local_vars.append({'name': 'solr_non_heap_' + nohu, 'timestamp': self.timestamp, 'value': nohuvalue, 'check_type': check_type})
+
             for tr in sothreads:
-                trvalue = stats_json['metrics'][3][tr]['value']
+                trvalue = stats_json['metrics']['solr.jvm'][tr]
                 self.local_vars.append({'name': 'solr_' + tr.replace('.', '_').replace('_count', ''), 'timestamp': self.timestamp, 'value': trvalue, 'check_type': check_type})
-    
+
             for gc in garbage:
-                if gc in stats_json['metrics'][3]:
-                    gcvalue = stats_json['metrics'][3][tr]['value']
+                if gc in stats_json['metrics']['solr.jvm']:
+                    gcvalue = stats_json['metrics']['solr.jvm'][gc]
                     self.local_vars.append({'name': 'solr_' + gc.replace('.', '_').replace('ConcurrentMarkSweep', 'CMS').lower(), 'timestamp': self.timestamp, 'value': gcvalue, 'check_type': check_type})
     
         except Exception as e:
