556f48e3900542150c5fd0089af364c03460407a
Ara Sadoyan <ara.sadoyan@netangels.net>
2018-02-14 19:06:40 +0400

    Couchbase update

--- checks_available/check_couchdb_2x.py
+++ checks_available/check_couchdb_2x.py
@@ -14,8 +14,6 @@ detailed = lib.getconfig.getparam('CouchDB2', 'detailed')
 cluster_name = lib.getconfig.getparam('SelfConfig', 'cluster_name')
 check_type = 'couchdb'
 
-codes = ['200','201','202','204','206','301','302','304','400','401','403','404','405','406','409','412','413','414','415','416','417','500','501','503']
-methods = ['COPY', 'DELETE', 'GET', 'HEAD', 'OPTIONS', 'POST', 'PUT']
 dbops = ['database_writes', 'database_reads', 'document_inserts', 'document_writes']
 httpd = ['bulk_requests', 'requests', 'temporary_view_reads', 'view_reads']
 
@@ -30,6 +28,7 @@ class Check(lib.basecheck.CheckBase):
             for couch in memberslist['all_nodes']:
                 try:
                     nodename = socket.gethostbyaddr(couch.split('@')[1])[0]
+
                 except:
                     nodename = couch.split('@')[1]
 
@@ -42,12 +41,11 @@ class Check(lib.basecheck.CheckBase):
                 for h in httpd:
                     reqrate = self.rate.record_value_rate(nodename + '_couchdb_' + h, metrics['couchdb']['httpd'][h]['value'], self.timestamp)
                     self.local_vars.append({'name': 'couchdb_' + h, 'host': nodename, 'timestamp': self.timestamp, 'value': int(reqrate), 'check_type': check_type})
-
                 if detailed:
-                    for m in methods:
+                    for m in metrics['couchdb']['httpd_request_methods'].keys():
                         reqrate = self.rate.record_value_rate(nodename + '_couchdb_' + m, metrics['couchdb']['httpd_request_methods'][m]['value'], self.timestamp)
                         self.local_vars.append({'name': 'couchdb_requests_methods', 'host': nodename, 'timestamp': self.timestamp, 'value': int(reqrate), 'check_type': check_type, 'extra_tag': {'method': m.lower()}})
-                    for c in codes:
+                    for c in metrics['couchdb']['httpd_status_codes'].keys():
                         reqrate = self.rate.record_value_rate(nodename + '_couchdb_' + str(c), metrics['couchdb']['httpd_status_codes'][c]['value'], self.timestamp)
                         self.local_vars.append({'name': 'couchdb_status_codes', 'host': nodename, 'timestamp': self.timestamp, 'value': int(reqrate), 'check_type': check_type, 'extra_tag': {'method': c}})
 
