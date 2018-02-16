e6083e5a89c2c8086aa4c1eedc64751900272f95
Ara Sadoyan <ara.sadoyan@netangels.net>
2018-02-16 18:31:31 +0400

    NameNode and HMaster checks important update

--- checks_available/check_hbase_master.py
+++ checks_available/check_hbase_master.py
@@ -7,23 +7,22 @@ import json
 
 
 hbase_master_url = lib.getconfig.getparam('HBase-Master', 'jmx')
-# cluster_name = lib.getconfig.getparam('SelfConfig', 'cluster_name')
 check_type = 'hbase'
 
 class Check(lib.basecheck.CheckBase):
 
     def precheck(self):
-        # local_vars = []
         try:
             stats_json = json.loads(lib.commonclient.httpget(__name__, hbase_master_url))
             stats_keys = stats_json['beans']
             node_rated_keys = ('clusterRequests','GcTimeMillis')
-            node_stuck_keys = ('GcCount','HeapMemoryUsage')
-            # rate = lib.record_rate.ValueRate()
-            # timestamp = int(datetime.datetime.now().strftime("%s"))
-    
+            node_stuck_keys = ('GcCount','HeapMemoryUsage', 'numRegionServers', 'numDeadRegionServers',
+                               'ritOldestAge', 'ritCountOverThreshold', 'ritCount', 'averageLoad', 'numRegionServers', 'numDeadRegionServers')
+            exceptions = ('exceptions.RegionMovedException', 'exceptions.multiResponseTooLarge', 'exceptions.RegionTooBusyException',
+                          'exceptions.FailedSanityCheckException', 'exceptions.UnknownScannerException',
+                          'exceptions.OutOfOrderScannerNextException', 'exceptions')
             for stats_x in range(0, len(stats_keys)):
-                for k, v in enumerate(('java.lang:type = GarbageCollector,name = ConcurrentMarkSweep', 'java.lang:type = GarbageCollector,name = ParNew')):
+                for k, v in enumerate(('java.lang:type=GarbageCollector,name=ConcurrentMarkSweep', 'java.lang:type=GarbageCollector,name=ParNew')):
                     if v in stats_keys[stats_x]['name']:
                         if k is 0:
                             cms_key = 'hmaster_heap_cms_lastgcInfo'
@@ -35,7 +34,7 @@ class Check(lib.basecheck.CheckBase):
                             self.local_vars.append({'name': parnew_key, 'timestamp': self.timestamp, 'value': parnew_value, 'check_type': check_type})
     
             for stats_x in range(0, len(stats_keys)):
-                for k, v in enumerate(('java.lang:type = GarbageCollector,name = G1 Young Generation', 'java.lang:type = GarbageCollector,name = G1 Old Generation')):
+                for k, v in enumerate(('java.lang:type=GarbageCollector,name=G1 Young Generation', 'java.lang:type=GarbageCollector,name=G1 Old Generation')):
                     if v in stats_keys[stats_x]['name']:
                         if k is 0:
                             g1_young_key = 'hmaster_heap_g1_young_lastgcInfo'
@@ -60,8 +59,7 @@ class Check(lib.basecheck.CheckBase):
                             values_rate = self.rate.record_value_rate('hmaster_'+values, myvalue, self.timestamp)
                             if values_rate >= 0:
                                 self.local_vars.append({'name': 'hmaster_node_' + values.lower(), 'timestamp': self.timestamp, 'value': values_rate, 'check_type': check_type, 'chart_type': 'Rate'})
-    
-    
+
                 for values in node_stuck_keys:
                     if values in stats_keys[stats_index]:
                         if values == 'HeapMemoryUsage':
@@ -71,9 +69,14 @@ class Check(lib.basecheck.CheckBase):
                         elif values == 'GcCount':
                             self.local_vars.append({'name': 'hmaster_node_' + values.lower(), 'timestamp': self.timestamp, 'value': stats_keys[stats_index][values], 'check_type': check_type, 'reaction': -3})
                         else:
-                            self.local_vars.append({'name': 'hmaster_node_' + values.lower(), 'timestamp': self.timestamp, 'value': stats_keys[stats_index][values], 'check_type': check_type, 'chart_type': 'Rate'})
-    
-            # return local_vars
+                            self.local_vars.append({'name': 'hmaster_node_' + values.lower(), 'timestamp': self.timestamp, 'value': stats_keys[stats_index][values], 'check_type': check_type})
+
+                for values in exceptions:
+                    if values in stats_keys[stats_index]:
+                        vv = values.lower().replace('.', '_').replace('exceptions', '').replace('exception', '')
+                        self.local_vars.append({'name': 'hmaster_exceptions' + vv, 'timestamp': self.timestamp, 'value': stats_keys[stats_index][values], 'check_type': check_type})
+
+
         except Exception as e:
             lib.puylogger.print_message(__name__ + ' Error : ' + str(e))
             pass
