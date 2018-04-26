0f4e854c92b1d3b5e93f14f213cbdd24fb874448
Ara Sadoyan <ara.sadoyan@netangels.net>
2018-04-26 18:28:13 +0400

    Added Ceph health check as special metric

--- checks_available/check_ceph.py
+++ checks_available/check_ceph.py
@@ -13,14 +13,18 @@ class Check(lib.basecheck.CheckBase):
 
     def precheck(self):
         try:
-            command = 'ceph -n ' + ceph_client + ' --keyring=' + ceph_keyring + ' -s -f json'
+            command1 = 'ceph -n ' + ceph_client + ' --keyring=' + ceph_keyring + ' -s -f json'
+            command2 = 'ceph -n ' + ceph_client + ' --keyring=' + ceph_keyring + ' pg stat -f json'
 
-            p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, universal_newlines=True)
-            output, err = p.communicate()
-            s = json.loads(output)
-            pgstats=s['pgmap']
-            health = s['health']
+            p1 = subprocess.Popen(command1, stdout=subprocess.PIPE, shell=True, universal_newlines=True)
+            output1, err = p1.communicate()
+            s1 = json.loads(output1)
+            stats=s1['pgmap']
+            health = s1['health']
 
+            p2 = subprocess.Popen(command2, stdout=subprocess.PIPE, shell=True, universal_newlines=True)
+            output2, err = p2.communicate()
+            pgstats = json.loads(output2)
 
             if health['overall_status'] == 'HEALTH_OK':
                 health_value = 0
@@ -38,42 +42,47 @@ class Check(lib.basecheck.CheckBase):
 
             self.jsondata.send_special("Ceph-Health", self.timestamp, health_value, message, err_type)
 
-            self.local_vars.append({'name': 'ceph_bytes_avail', 'timestamp': self.timestamp, 'value': pgstats['bytes_avail'], 'check_type': check_type})
-            self.local_vars.append({'name': 'ceph_bytes_total', 'timestamp': self.timestamp, 'value': pgstats['bytes_total'], 'check_type': check_type})
-            self.local_vars.append({'name': 'ceph_bytes_used', 'timestamp': self.timestamp, 'value': pgstats['bytes_used'], 'check_type': check_type})
-            self.local_vars.append({'name': 'ceph_data_bytes', 'timestamp': self.timestamp, 'value': pgstats['data_bytes'], 'check_type': check_type})
-            self.local_vars.append({'name': 'ceph_num_pgs', 'timestamp': self.timestamp, 'value': pgstats['num_pgs'], 'check_type': check_type})
+            self.local_vars.append({'name': 'ceph_bytes_avail', 'timestamp': self.timestamp, 'value': stats['bytes_avail'], 'check_type': check_type})
+            self.local_vars.append({'name': 'ceph_bytes_total', 'timestamp': self.timestamp, 'value': stats['bytes_total'], 'check_type': check_type})
+            self.local_vars.append({'name': 'ceph_bytes_used', 'timestamp': self.timestamp, 'value': stats['bytes_used'], 'check_type': check_type})
+            self.local_vars.append({'name': 'ceph_data_bytes', 'timestamp': self.timestamp, 'value': stats['data_bytes'], 'check_type': check_type})
+            self.local_vars.append({'name': 'ceph_num_pgs', 'timestamp': self.timestamp, 'value': stats['num_pgs'], 'check_type': check_type})
 
-            if 'read_bytes_sec' in pgstats:
-                self.local_vars.append({'name': 'ceph_read_bytes_sec', 'timestamp': self.timestamp, 'value': pgstats['read_bytes_sec'], 'check_type': check_type, 'chart_type': 'Rate'})
+            if 'io_sec' in pgstats:
+                self.local_vars.append({'name': 'ceph_io_sec', 'timestamp': self.timestamp, 'value': pgstats['io_sec'], 'check_type': check_type, 'chart_type': 'Rate'})
+            else:
+                self.local_vars.append({'name': 'ceph_io_sec', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type, 'chart_type': 'Rate'})
+
+            if 'read_bytes_sec' in stats:
+                self.local_vars.append({'name': 'ceph_read_bytes_sec', 'timestamp': self.timestamp, 'value': stats['read_bytes_sec'], 'check_type': check_type, 'chart_type': 'Rate'})
             else:
                 self.local_vars.append({'name': 'ceph_read_bytes_sec', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type, 'chart_type': 'Rate'})
-            if 'write_bytes_sec' in pgstats:
-                self.local_vars.append({'name': 'ceph_write_bytes_sec', 'timestamp': self.timestamp, 'value': pgstats['write_bytes_sec'], 'check_type': check_type, 'chart_type': 'Rate'})
+            if 'write_bytes_sec' in stats:
+                self.local_vars.append({'name': 'ceph_write_bytes_sec', 'timestamp': self.timestamp, 'value': stats['write_bytes_sec'], 'check_type': check_type, 'chart_type': 'Rate'})
             else:
                 self.local_vars.append({'name': 'ceph_write_bytes_sec', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type, 'chart_type': 'Rate'})
-            if 'degraded_objects' in pgstats:
-                self.local_vars.append({'name': 'ceph_degraded_objects', 'timestamp': self.timestamp, 'value': pgstats['degraded_objects'], 'check_type': check_type})
+            if 'degraded_objects' in stats:
+                self.local_vars.append({'name': 'ceph_degraded_objects', 'timestamp': self.timestamp, 'value': stats['degraded_objects'], 'check_type': check_type})
             else:
                 self.local_vars.append({'name': 'ceph_degraded_objects', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type})
-            if 'degraded_ratio' in pgstats:
-                self.local_vars.append({'name': 'ceph_degraded_ratio', 'timestamp': self.timestamp, 'value': pgstats['degraded_ratio'], 'check_type': check_type})
+            if 'degraded_ratio' in stats:
+                self.local_vars.append({'name': 'ceph_degraded_ratio', 'timestamp': self.timestamp, 'value': stats['degraded_ratio'], 'check_type': check_type})
             else:
                 self.local_vars.append({'name': 'ceph_degraded_ratio', 'timestamp': self.timestamp, 'value': 0.0, 'check_type': check_type})
-            if 'degraded_total' in pgstats:
-                self.local_vars.append({'name': 'ceph_degraded_total', 'timestamp': self.timestamp, 'value': pgstats['degraded_total'], 'check_type': check_type})
+            if 'degraded_total' in stats:
+                self.local_vars.append({'name': 'ceph_degraded_total', 'timestamp': self.timestamp, 'value': stats['degraded_total'], 'check_type': check_type})
             else:
                 self.local_vars.append({'name': 'ceph_degraded_total', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type})
-            if 'recovering_bytes_per_sec' in pgstats:
-                self.local_vars.append({'name': 'ceph_recovering_bytes_per_sec', 'timestamp': self.timestamp, 'value': pgstats['recovering_bytes_per_sec'], 'check_type': check_type})
+            if 'recovering_bytes_per_sec' in stats:
+                self.local_vars.append({'name': 'ceph_recovering_bytes_per_sec', 'timestamp': self.timestamp, 'value': stats['recovering_bytes_per_sec'], 'check_type': check_type})
             else:
                 self.local_vars.append({'name': 'ceph_recovering_bytes_per_sec', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type})
-            if 'num_objects_recovered' in pgstats:
-                self.local_vars.append({'name': 'ceph_num_objects_recovered', 'timestamp': self.timestamp, 'value': pgstats['num_objects_recovered'], 'check_type': check_type})
+            if 'num_objects_recovered' in stats:
+                self.local_vars.append({'name': 'ceph_num_objects_recovered', 'timestamp': self.timestamp, 'value': stats['num_objects_recovered'], 'check_type': check_type})
             else:
                 self.local_vars.append({'name': 'ceph_num_objects_recovered', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type})
-            if 'recovering_keys_per_sec' in pgstats:
-                self.local_vars.append({'name': 'ceph_recovering_keys_per_sec', 'timestamp': self.timestamp, 'value': pgstats['recovering_keys_per_sec'], 'check_type': check_type})
+            if 'recovering_keys_per_sec' in stats:
+                self.local_vars.append({'name': 'ceph_recovering_keys_per_sec', 'timestamp': self.timestamp, 'value': stats['recovering_keys_per_sec'], 'check_type': check_type})
             else:
                 self.local_vars.append({'name': 'ceph_recovering_keys_per_sec', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type})
 
