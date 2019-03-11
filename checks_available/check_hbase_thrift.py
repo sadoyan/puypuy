c93e7540fd6f19f7bfe93fd1d3fc628a6c932ce4
Ara Sadoyan <ara.sadoyan@netangels.net>
2019-03-11 16:38:05 +0400

    Refactor of HBase REST and Thrift checks

--- checks_available/check_hbase_thrift.py
+++ checks_available/check_hbase_thrift.py
@@ -1,154 +1,88 @@
 import lib.record_rate
-import lib.pushdata
-import lib.puylogger
-import lib.jolostart
 import lib.commonclient
+import lib.puylogger
 import lib.getconfig
 import lib.basecheck
 import json
 
-
-hthrift_url = lib.getconfig.getparam('HBase-Thrift', 'url')
-java = lib.getconfig.getparam('HBase-Thrift', 'java')
-juser = lib.getconfig.getparam('HBase-Thrift', 'user')
-jclass = lib.getconfig.getparam('HBase-Thrift', 'class')
+hbase_thrift_url = lib.getconfig.getparam('HBase-Thrift', 'jmx')
 check_type = 'hbase'
-reaction = -3
-
-lib.jolostart.do_joloikia(java, juser, jclass, hthrift_url)
 
 
 class Check(lib.basecheck.CheckBase):
 
     def precheck(self):
-    
         try:
-            data_dict = json.loads(lib.commonclient.httpget(__name__, hthrift_url + '/java.lang:type=GarbageCollector,name=*'))
-    
-            ConcurrentMarkSweep = 'java.lang:name=ConcurrentMarkSweep,type=GarbageCollector'
-            G1Gc = 'java.lang:name=G1 Young Generation,type=GarbageCollector'
-    
-            if ConcurrentMarkSweep in data_dict['value']:
-                CMS = True
-                G1 = False
-            elif G1Gc in data_dict['value']:
-                CMS = False
-                G1 = True
-            else:
-                CMS = False
-                G1 = False
-    
-            heam_mem = 'java.lang:type=Memory'
-            jolo_json = json.loads(lib.commonclient.httpget(__name__, hthrift_url + '/' + heam_mem))
-            jolo_keys = jolo_json['value']
-            metr_name = ('used', 'committed', 'max')
-            heap_type = ('NonHeapMemoryUsage', 'HeapMemoryUsage')
-            for heap in heap_type:
-                for metr in metr_name:
-                    if heap == 'NonHeapMemoryUsage':
-                        key = 'hthrift_nonheap_' + metr
-                        mon_values = jolo_keys[heap][metr]
-                        if metr == 'used':
-                            self.local_vars.append({'name': key, 'timestamp': self.timestamp, 'value': mon_values, 'check_type': check_type})
-                        else:
-                            self.local_vars.append({'name': key, 'timestamp': self.timestamp, 'value': mon_values, 'check_type': check_type, 'reaction': reaction})
-                    else:
-                        key = 'hthrift_heap_' + metr
-                        mon_values = jolo_keys[heap][metr]
-                        if metr == 'used':
-                            self.local_vars.append({'name': key, 'timestamp': self.timestamp, 'value': mon_values, 'check_type': check_type})
-                        else:
-                            self.local_vars.append({'name': key, 'timestamp': self.timestamp, 'value': mon_values, 'check_type': check_type, 'reaction': reaction})
-            if CMS is True:
-                collector = ('java.lang:name=ParNew,type=GarbageCollector', 'java.lang:name=ConcurrentMarkSweep,type=GarbageCollector')
-                for coltype in collector:
-                    beans = json.loads(lib.commonclient.httpget(__name__, hthrift_url + '/' + coltype))
-                    if beans['value']['LastGcInfo']:
-                        LastGcInfo = beans['value']['LastGcInfo']['duration']
-                    CollectionCount = beans['value']['CollectionCount']
-                    CollectionTime = beans['value']['CollectionTime']
-    
-                    def push_metrics(preffix):
-                        if 'LastGcInfo' in locals():
-                            self.local_vars.append({'name': 'hthrift_' + preffix + '_lastgcinfo', 'timestamp': self.timestamp, 'value': LastGcInfo, 'check_type': check_type})
-                        CollectionTime_rate = self.rate.record_value_rate('hthrift_' + preffix + '_collection_time', CollectionTime, self.timestamp)
-                        self.local_vars.append({'name': 'hthrift_' + preffix + '_collection_count', 'timestamp': self.timestamp, 'value': CollectionCount, 'check_type': check_type})
-                        self.local_vars.append({'name': 'hthrift_' + preffix + '_collection_time', 'timestamp': self.timestamp, 'value': CollectionTime_rate, 'check_type': check_type, 'chart_type': 'Rate'})
-    
-                    if coltype == 'java.lang:name=ConcurrentMarkSweep,type=GarbageCollector':
-                        push_metrics(preffix='cms')
-                    if coltype == 'java.lang:name=ParNew,type=GarbageCollector':
-                        push_metrics(preffix='parnew')
-    
-            if G1 is True:
-                gc_g1 = ('/java.lang:name=G1%20Old%20Generation,type=GarbageCollector', '/java.lang:name=G1%20Young%20Generation,type=GarbageCollector')
-    
-                def check_null(value):
-                    if value is None:
-                        value = 0
-                        return value
-                    else:
-                        return value
-    
-                for k, v in enumerate(gc_g1):
-                    j = json.loads(lib.commonclient.httpget(__name__, hthrift_url + v))
-                    name = 'LastGcInfo'
-                    if k is 0:
-                        try:
-                            value = j['value'][name]['duration']
-                            v = check_null(value)
-                        except:
-                            v = 0
-                            pass
-                        m_name = 'hthrift_g1_old_lastgcinfo'
-                    if k is 1:
-                        value = j['value'][name]['duration']
-                        v = check_null(value)
-                        m_name = 'hthrift_g1_young_Lastgcinfo'
-                    self.local_vars.append({'name': m_name, 'timestamp': self.timestamp, 'value': v, 'check_type': check_type})
-    
-                metr_keys = ('CollectionTime', 'CollectionCount')
-                for k, v in enumerate(gc_g1):
-                    j = json.loads(lib.commonclient.httpget(__name__, hthrift_url + v))
-                    if k is 0:
-                        type = '_old_'
-                    if k is 1:
-                        type = '_young_'
-                    for ky, vl in enumerate(metr_keys):
-                        if ky is 0:
-                            value = j['value'][vl]
-                            v = check_null(value)
-                            rate_key = vl + type
-                            CollectionTime_rate = self.rate.record_value_rate('hthrift_' + rate_key, v, self.timestamp)
-                            self.local_vars.append({'name': 'hthrift_g1' + type + vl, 'timestamp': self.timestamp, 'value': CollectionTime_rate, 'check_type': check_type, 'chart_type': 'Rate'})
-                        if ky is 1:
-                            value = j['value'][vl]
-                            v = check_null(value)
-                            self.local_vars.append({'name': 'hthrift_g1' + type + vl, 'timestamp': self.timestamp, 'value': v, 'check_type': check_type})
-            jolo_threads = 'java.lang:type=Threading'
-            jolo_tjson = json.loads(lib.commonclient.httpget(__name__, hthrift_url + '/' + jolo_threads))
+            stats_json = json.loads(lib.commonclient.httpget(__name__, hbase_thrift_url))
+            stats_keys = stats_json['beans']
+            for stats_x in range(0, len(stats_keys)):
+                for k, v in enumerate(('java.lang:type=GarbageCollector,name=ConcurrentMarkSweep', 'java.lang:type=GarbageCollector,name=ParNew')):
+                    if v in stats_keys[stats_x]['name']:
+                        if k is 0:
+                            if stats_keys[stats_x]['LastGcInfo'] is not None:
+                                cms_key = 'hthrift_cms_lastgcinfo'
+                                cms_value = stats_keys[stats_x]['LastGcInfo']['duration']
+                                self.local_vars.append({'name': cms_key, 'timestamp': self.timestamp, 'value': cms_value, 'check_type': check_type})
+                            else:
+                                self.local_vars.append({'name': 'hthrift_cms_lastgcinfo', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type})
+                        if k is 1:
+                            if stats_keys[stats_x]['LastGcInfo'] is not None:
+                                parnew_key = 'hthrift_parnew_lastgcinfo'
+                                parnew_value = stats_keys[stats_x]['LastGcInfo']['duration']
+                                self.local_vars.append({'name': parnew_key, 'timestamp': self.timestamp, 'value': parnew_value, 'check_type': check_type})
+                            else:
+                                self.local_vars.append({'name': 'hthrift_parnew_lastgcinfo', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type})
+
+            for stats_x in range(0, len(stats_keys)):
+                for k, v in enumerate(('java.lang:type=GarbageCollector,name=G1 Young Generation', 'java.lang:type=GarbageCollector,name=G1 Old Generation')):
+                    if v in stats_keys[stats_x]['name']:
+                        if k is 0:
+                            if stats_keys[stats_x]['LastGcInfo'] is not None:
+                                g1_young_key = 'hthrift_g1_young_lastgcinfo'
+                                g1_young_value = stats_keys[stats_x]['LastGcInfo']['duration']
+                                self.local_vars.append({'name': g1_young_key, 'timestamp': self.timestamp, 'value': g1_young_value, 'check_type': check_type})
+                            else:
+                                self.local_vars.append({'name': 'hthrift_g1_old_lastgcinfo', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type})
+                        if k is 1:
+                            if stats_keys[stats_x]['LastGcInfo'] is not None:
+                                g1_old_key = 'hthrift_g1_old_lastgcinfo'
+                                g1_old_value = stats_keys[stats_x]['LastGcInfo']['duration']
+                                self.local_vars.append({'name': g1_old_key, 'timestamp': self.timestamp, 'value': g1_old_value, 'check_type': check_type})
+                            else:
+                                self.local_vars.append({'name': 'hthrift_g1_old_lastgcinfo', 'timestamp': self.timestamp, 'value': 0, 'check_type': check_type})
+
             thread_metrics = ('TotalStartedThreadCount', 'PeakThreadCount', 'ThreadCount', 'DaemonThreadCount')
-            for thread_metric in thread_metrics:
-                name = 'hthrift_' + thread_metric.lower()
-                vlor = jolo_tjson['value'][thread_metric]
-                self.local_vars.append({'name': name, 'timestamp': self.timestamp, 'value': vlor, 'check_type': check_type})
-    
-            jolo_thrift = 'hadoop:service=thrift,name=Thrift'
-            jolo_tjson = json.loads(lib.commonclient.httpget(__name__, hthrift_url + '/' + jolo_thrift))
-            hrmetrics = ('CorePoolSize', 'CountersMapSize', 'FailedIncrements', 'TotalIncrements',
-                         'MaxPoolSize', 'MaxQueueSize', 'PoolCompletedTaskCount', 'PoolLargestPoolSize', 'QueueSize'
-                         )
-            for thread_metric in hrmetrics:
-                name = 'hthrift_' + thread_metric.lower()
-                blor = jolo_tjson['value'][thread_metric]
-                self.local_vars.append({'name': name, 'timestamp': self.timestamp, 'value': blor, 'check_type': check_type})
-    
+            for index in range(0, len(stats_keys)):
+                if stats_keys[index]['name'] == 'java.lang:type=Threading':
+                    for thread_metric in thread_metrics:
+                        self.local_vars.append({'name': 'hthrift_' + thread_metric.lower(), 'timestamp': self.timestamp, 'value': stats_keys[index][thread_metric], 'check_type': check_type})
+                if stats_keys[index]['name'] == 'java.lang:type=Memory':
+                    heap_metrics=('max', 'init', 'committed', 'used')
+                    for heap_value in heap_metrics:
+                        self.local_vars.append({'name': 'hthrift_heap_' + heap_value, 'timestamp': self.timestamp, 'value': stats_keys[index]['HeapMemoryUsage'][heap_value], 'check_type': check_type})
+
+                if stats_keys[index]['name'] == 'Hadoop:service=HBase,name=Thrift,sub=ThriftOne':
+                    hrmetrics = ('PauseTimeWithGc_99th_percentile', 'PauseTimeWithGc_90th_percentile',
+                                 'PauseTimeWithoutGc_90th_percentile', 'PauseTimeWithoutGc_99th_percentile', 'callQueueLen', 'TimeInQueue_num_ops')
+                    hrmetrics_rated = ('BatchMutate_num_ops', 'ThriftCall_num_ops', 'SlowThriftCall_num_ops', 'BatchGet_num_ops', )
+                    for hrmetric in hrmetrics:
+                        self.local_vars.append({'name': 'hthrift_' + hrmetric.lower(), 'timestamp': self.timestamp, 'value': stats_keys[index][hrmetric], 'check_type': check_type, 'extra_tag': {'thrift_queue': 'one'}})
+
+                    for hrmetric_rated in hrmetrics_rated:
+                        bolor = self.rate.record_value_rate('hthrift_one' + hrmetric_rated, stats_keys[index][hrmetric_rated], self.timestamp)
+                        self.local_vars.append({'name': 'hthrift_' + hrmetric_rated.replace('_num_ops', '').lower(), 'timestamp': self.timestamp, 'value': bolor, 'check_type': check_type, 'chart_type': 'Rate', 'extra_tag': {'thrift_queue': 'one'}})
+
+                if stats_keys[index]['name'] == 'Hadoop:service=HBase,name=Thrift,sub=ThriftTwo':
+                    hrmetrics = ('PauseTimeWithGc_99th_percentile', 'PauseTimeWithGc_90th_percentile',
+                                 'PauseTimeWithoutGc_90th_percentile', 'PauseTimeWithoutGc_99th_percentile', 'callQueueLen', 'TimeInQueue_num_ops')
+                    hrmetrics_rated = ('BatchMutate_num_ops', 'ThriftCall_num_ops', 'SlowThriftCall_num_ops', 'BatchGet_num_ops', )
+                    for hrmetric in hrmetrics:
+                        self.local_vars.append({'name': 'hthrift_' + hrmetric.lower(), 'timestamp': self.timestamp, 'value': stats_keys[index][hrmetric], 'check_type': check_type, 'extra_tag': {'thrift_queue': 'two'}})
+
+                    for hrmetric_rated in hrmetrics_rated:
+                        bolor = self.rate.record_value_rate('hthrift_two' + hrmetric_rated, stats_keys[index][hrmetric_rated], self.timestamp)
+                        self.local_vars.append({'name': 'hthrift_' + hrmetric_rated.replace('_num_ops', '').lower(), 'timestamp': self.timestamp, 'value': bolor, 'check_type': check_type, 'chart_type': 'Rate', 'extra_tag': {'thrift_queue': 'two'}})
         except Exception as e:
             lib.puylogger.print_message(__name__ + ' Error : ' + str(e))
-            try:
-                lib.jolostart.do_joloikia(java, juser, jclass, hthrift_url)
-            except Exception as jolo:
-                lib.pushdata.print_error(__name__, (jolo))
             pass
-    
-    
+
