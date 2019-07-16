bab19a4351ef8aa350fec890f9de7cd89dd250b7
Ara Sadoyan <ara.sadoyan@netangels.net>
2019-07-16 19:26:28 +0400

    minor update Check for etcd

--- checks_available/check_etcd.py
+++ checks_available/check_etcd.py
@@ -7,7 +7,7 @@ import lib.basecheck
 metrics = lib.getconfig.getparam('etcd', 'metrics')
 check_type = 'etcd'
 greps = ('sum', 'count', 'process')
-reaction  = 0
+reaction = 0
 
 class Check(lib.basecheck.CheckBase):
 
@@ -18,9 +18,9 @@ class Check(lib.basecheck.CheckBase):
                 if any(x in c for x in greps) and '#' not in c:
                     o = c.split()
                     if c.startswith('etcd_'):
-                        self.local_vars.append({'name': o[0], 'timestamp': self.timestamp, 'value': o[1], 'reaction': reaction})
+                        self.local_vars.append({'name': o[0], 'timestamp': self.timestamp, 'value': float(o[1]), 'reaction': reaction})
                     else:
-                        self.local_vars.append({'name': 'etcd_' + o[0], 'timestamp': self.timestamp, 'value': o[1], 'reaction': reaction})
+                        self.local_vars.append({'name': 'etcd_' + o[0], 'timestamp': self.timestamp, 'value': float(o[1]), 'reaction': reaction})
         except Exception as e:
             lib.puylogger.print_message(__name__ + ' Error : ' + str(e))
             pass
