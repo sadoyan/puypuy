f4fe5ffa7798d6fdff28a20f5c2b28af5c01db48
Ara Sadoyan <ara.sadoyan@netangels.net>
2019-10-31 15:49:56 +0400

    HAProxy check improvements

--- checks_available/check_haproxy.py
+++ checks_available/check_haproxy.py
@@ -4,34 +4,35 @@ import lib.commonclient
 import lib.puylogger
 import lib.basecheck
 
-
 haproxy_url = lib.getconfig.getparam('HAProxy', 'url')
 haproxy_auth = lib.getconfig.getparam('HAProxy', 'user')+':'+lib.getconfig.getparam('HAProxy', 'pass')
 curl_auth = lib.getconfig.getparam('HAProxy', 'auth')
-haproxy_upstream = lib.getconfig.getparam('HAProxy', 'upstream').split(',')
 check_type = 'haproxy'
+
 
 class Check(lib.basecheck.CheckBase):
 
     def precheck(self):
-
         try:
             if curl_auth is True:
                 t = lib.commonclient.httpget(__name__, haproxy_url, haproxy_auth)
             else:
                 t = lib.commonclient.httpget(__name__, haproxy_url)
-            lazy_totals = ("FRONTEND", "BACKEND")
             for line in t.split('\n'):
-                for application in haproxy_upstream:
-                    if application in line:
-                        if not any(s in line for s in lazy_totals):
-                            upstream = line.split(',')[1]
-                            sessions = line.split(',')[4]
-                            connrate = line.split(',')[33]
-                            self.local_vars.append({'name': 'haproxy_connrate_' + upstream, 'timestamp': self.timestamp, 'value': connrate, 'check_type': check_type, 'chart_type': 'Rate'})
-                            self.local_vars.append({'name': 'haproxy_sessions_' + upstream, 'timestamp': self.timestamp, 'value': sessions, 'check_type': check_type})
+                        ddd = line.split(',')
+                        if 'pxname,svname' not in line and len(ddd) > 1:
+                            if ddd[1] == 'FRONTEND':
+                                self.local_vars.append({'name': 'haproxy_connrate', 'timestamp': self.timestamp, 'value': ddd[33], 'check_type': check_type, 'chart_type': 'Rate', 'extra_tag': {'frontend': ddd[0]}})
+                                self.local_vars.append({'name': 'haproxy_sessions', 'timestamp': self.timestamp, 'value': ddd[4], 'check_type': check_type, 'extra_tag': {'frontend': ddd[0]}})
+                            elif ddd[1] == 'BACKEND':
+                                self.local_vars.append({'name': 'haproxy_queue', 'timestamp': self.timestamp, 'value': ddd[2], 'check_type': check_type, 'extra_tag': {'backend': ddd[0]}})
+                                self.local_vars.append({'name': 'haproxy_connrate', 'timestamp': self.timestamp, 'value': ddd[33], 'check_type': check_type, 'chart_type': 'Rate', 'extra_tag': {'backend': ddd[0]}})
+                                self.local_vars.append({'name': 'haproxy_sessions', 'timestamp': self.timestamp, 'value': ddd[4], 'check_type': check_type, 'extra_tag': {'backend': ddd[0]}})
+                            else:
+                                upstream = ddd[1]
+                                self.local_vars.append({'name': 'haproxy_queue', 'timestamp': self.timestamp, 'value': ddd[2], 'check_type': check_type, 'extra_tag': {'upstream': upstream}})
+                                self.local_vars.append({'name': 'haproxy_connrate', 'timestamp': self.timestamp, 'value': ddd[33], 'check_type': check_type, 'chart_type': 'Rate', 'extra_tag': {'upstream': upstream}})
+                                self.local_vars.append({'name': 'haproxy_sessions', 'timestamp': self.timestamp, 'value': ddd[4], 'check_type': check_type, 'extra_tag': {'upstream': upstream}})
         except Exception as e:
             lib.puylogger.print_message(__name__ + ' Error : ' + str(e))
             pass
-
-
