5c404aac52cca69541c1606f28261a74653ee7e3
Ara Sadoyan <ara.sadoyan@netangels.net>
2021-04-01 19:02:40 +0400

    Zookeeper fix

--- checks_available/check_zookeeper.py
+++ checks_available/check_zookeeper.py
@@ -8,6 +8,7 @@ zk_host = lib.getconfig.getparam('ZooKeeper', 'host')
 zk_port = int(lib.getconfig.getparam('ZooKeeper', 'port'))
 check_type = 'zookeeper'
 
+
 buffer_size = 2048
 message = 'mntr'
 
@@ -20,14 +21,16 @@ class Check(lib.basecheck.CheckBase):
             raw_data = lib.commonclient.socketget(__name__, buffer_size, zk_host, zk_port, message)
             if raw_data is not None:
                 for line in raw_data.splitlines():
-                    key = line.split('\t')[0]
-                    value = line.split('\t')[1]
-                    if value.isdigit():
-                        if key == 'zk_packets_received' or key == 'zk_packets_sent':
-                            value_rate = rate.record_value_rate(key, value, self.timestamp)
-                            self.local_vars.append({'name': key, 'timestamp': self.timestamp, 'value': value_rate, 'chart_type': 'Rate', 'check_type': check_type})
-                        else:
-                            self.local_vars.append({'name': key, 'timestamp': self.timestamp, 'value': value, 'check_type': check_type})
+                    kv = line.split('\t')
+                    if len(kv) > 1:
+                        key = kv[0]
+                        value = kv[1]
+                        if value.isdigit():
+                            if key == 'zk_packets_received' or key == 'zk_packets_sent':
+                                value_rate = rate.record_value_rate(key, value, self.timestamp)
+                                self.local_vars.append({'name': key, 'timestamp': self.timestamp, 'value': value_rate, 'chart_type': 'Rate', 'check_type': check_type})
+                            else:
+                                self.local_vars.append({'name': key, 'timestamp': self.timestamp, 'value': value, 'check_type': check_type})
         except Exception as e:
             lib.puylogger.print_message(__name__ + ' Error : ' + str(e))
             pass
