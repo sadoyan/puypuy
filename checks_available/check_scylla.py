0a8bfbd73260d8a6778755071f8bd7e8732f13c6
Ara Sadoyan <ara.sadoyan@netangels.net>
2025-03-28 14:06:28 +0100

    Scylla update

--- checks_available/check_scylla.py
+++ checks_available/check_scylla.py
@@ -10,17 +10,20 @@ check_type = 'scylladb'
 reaction = 0
 
 greps = [
-        'scylla_node_operation_mode', 'scylla_reactor_utilization', 'scylla_transport_requests_served',
+        'scylla_node_operation_mode', 'scylla_reactor_utilization',
         'scylla_storage_proxy_coordinator_write_latency_count', 'scylla_storage_proxy_coordinator_write_latency_sum',
         'scylla_storage_proxy_coordinator_write_timeouts', 'scylla_storage_proxy_coordinator_read_latency_count',
-        'scylla_storage_proxy_coordinator_read_latency_sum', 'scylla_storage_proxy_coordinator_read_timeouts', 'scylla_cache_row_hits', 'scylla_cache_row_misses',
+        'scylla_storage_proxy_coordinator_read_latency_sum', 'scylla_storage_proxy_coordinator_read_timeouts',
         'scylla_cache_bytes_total', 'scylla_cache_bytes_used', 'scylla_cache_concurrent_misses_same_key',
         'scylla_sstables_row_reads', 'scylla_sstables_range_partition_reads', 'scylla_sstables_range_tombstone_writes', 'scylla_sstables_row_writes',
         'scylla_sstables_single_partition_reads', 'scylla_sstables_sstable_partition_reads', 'scylla_sstables_static_row_writes', 'scylla_sstables_tombstone_writes',
         'scylla_transport_current_connections', 'scylla_cql_statements_in_batches'
     ]
 
-commands = ['scylla_cql_inserts{', 'scylla_cql_reads{', 'scylla_cql_deletes{', 'scylla_cql_updates{', 'scylla_cql_batches{']
+commands = [
+    'scylla_cql_inserts{', 'scylla_cql_reads{', 'scylla_cql_deletes{', 'scylla_cql_updates{', 'scylla_cql_batches{',
+    'scylla_cache_row_hits', 'scylla_cache_row_misses',  'scylla_transport_requests_served'
+    ]
 
 sgnames = {'main', 'statement', 'compaction'}
 
@@ -61,6 +64,7 @@ class Check(lib.basecheck.CheckBase):
                 for vl in v:
                     cnt = cnt + vl
                 bolor = self.rate.record_value_rate(k, cnt, self.timestamp)
+                # lib.puylogger.print_message(k + " " + str(bolor))
                 self.local_vars.append({'name': k, 'timestamp': self.timestamp, 'value': int(bolor), 'reaction': reaction, 'check_type': check_type})
             for k, v in tempo.items():
                 if type(v) is float:
