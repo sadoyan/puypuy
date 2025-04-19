9dbfd838ab4ddc820c13e98ceb084c905affee5c
Ara Sadoyan <ara.sadoyan@netangels.net>
2025-04-19 15:27:17 +0200

    Logs, and other updates

--- checks_available/check_twemproxy.py
+++ checks_available/check_twemproxy.py
@@ -17,10 +17,8 @@ class Check(lib.basecheck.CheckBase):
     def precheck(self):
         try:
             ratedmetrics = {"requests", "request_bytes", "responses", "response_bytes"}
-            rawmetrics = {"in_queue", "in_queue_bytes"}
+            rawmetrics = {"in_queue", "in_queue_bytes", "server_connections"}
             raw_data = json.loads(lib.commonclient.socketget(__name__, buffer_size, redis_host, redis_port, ''))
-            # lib.puylogger.print_message(raw_data["curr_connections"])
-            # lib.puylogger.print_message(raw_data["proxy"]["client_connections"])
             for k, v in raw_data["proxy"].items():
                 if isinstance(v, dict):
                     for key, value in v.items():
@@ -29,6 +27,7 @@ class Check(lib.basecheck.CheckBase):
                             self.local_vars.append({'name': 'twemproxy_' + key, 'timestamp': self.timestamp, 'value': reqrate, 'check_type': check_type, 'extra_tag': {'backend': k}})
                         elif key in rawmetrics:
                             self.local_vars.append({'name': 'twemproxy_' + key, 'timestamp': self.timestamp, 'value': value, 'check_type': check_type, 'extra_tag': {'backend': k}})
+                            lib.puylogger.print_message(key + " " + str(value) + " " + k)
             self.local_vars.append({'name': 'twemproxy_current_connections', 'timestamp': self.timestamp, 'value': raw_data["curr_connections"], 'check_type': check_type})
             self.local_vars.append({'name': 'twemproxy_client_connections', 'timestamp': self.timestamp, 'value': raw_data["proxy"]["client_connections"], 'check_type': check_type})
         except Exception as e:
