6d5873314caf47457eae31728a2209811e2bb17a
Ara Sadoyan <ara.sadoyan@netangels.net>
2025-06-07 15:10:23 +0200

    New checks

--- checks_available/check_gazan.py
+++ checks_available/check_gazan.py
@@ -5,20 +5,21 @@ import lib.puylogger
 import lib.basecheck
 import re
 
-# metrics = lib.getconfig.getparam('Traefik', 'metrics')
-metrics = "http://g01:3000/metrics"
+metrics = lib.getconfig.getparam('Gazan', 'metrics')
+# metrics = "http://127.0.0.1:3000/metrics"
 check_type = 'gazan'
 greps = ('sum', 'count', 'process')
 reaction = 0
 
-rated = {'gazan_requests_by_method_total', 'gazan_requests_total', 'gazan_responses_total'}
-mat = {'gazan_response_latency_seconds_count', 'gazan_response_latency_seconds_sum'}
-class Check(lib.basecheck.CheckBase):
+rated = {'gazan_requests_by_method_total', 'gazan_requests_total', 'gazan_responses_total', 'gazan_requests_by_version_total'}
+mat = {'gazan_request_latency_seconds_bucket', 'gazan_response_latency_seconds_bucket'}
 
+class Check(lib.basecheck.CheckBase):
     def precheck(self):
         try:
             data = lib.commonclient.httpget(__name__, metrics).splitlines()
-            lat_mat = {}
+            request = { }
+            response = {}
             for c in data:
                 if '#' not in c:
                     a = re.split(r'[{|} \s]+', c.replace('"', ''))
@@ -27,23 +28,56 @@ class Check(lib.basecheck.CheckBase):
                         if a[0] in rated:
                             for k, v in d.items():
                                 value = self.rate.record_value_rate(a[0]+k+v, a[-1], self.timestamp)
-                                lib.puylogger.print_message({'name': a[0], 'timestamp': self.timestamp, 'value': value, 'reaction': reaction, 'check_type': check_type,'extra_tag': {k: v}})
+                                name = a[0].replace("_total", "")
+                                self.local_vars.append({'name': name, 'timestamp': self.timestamp, 'value': value, 'reaction': reaction, 'check_type': check_type,'extra_tag': {k: v}})
+                        elif a[0] == "gazan_request_latency_seconds_bucket":
+                            for k, v in d.items():
+                                request[v] = float(a[-1])
+                        elif a[0] == "gazan_response_latency_seconds_bucket":
+                            for k, v in d.items():
+                                response[v] = float(a[-1])
                         else:
                             for k,v in d.items():
-                                lib.puylogger.print_message({'name': a[0], 'timestamp': self.timestamp, 'value': a[-1], 'reaction': reaction, 'check_type': check_type,'extra_tag': {k: v}})
+                                self.local_vars.append({'name': a[0], 'timestamp': self.timestamp, 'value': a[-1], 'reaction': reaction, 'check_type': check_type,'extra_tag': {k: v}})
                     else:
-                        if a[0] in mat:
-                            lat_mat[a[0]]=float(a[-1])
                         if a[0] in rated:
                             value = self.rate.record_value_rate(a[0], a[-1], self.timestamp)
-                            lib.puylogger.print_message(' ====== name', a[0], 'value', value, "======")
-                            # lib.puylogger.print_message({'name': a[0], 'timestamp': self.timestamp, 'value': value, 'reaction': reaction})
+                            name = a[0].replace("_total", "")
+                            self.local_vars.append({'name': name, 'timestamp': self.timestamp, 'value': value, 'reaction': reaction})
                         else:
-                            lib.puylogger.print_message({'name': a[0], 'timestamp': self.timestamp, 'value': a[-1], 'reaction': reaction})
-            lat = lat_mat['gazan_response_latency_seconds_sum']/lat_mat['gazan_response_latency_seconds_count']
-            lib.puylogger.print_message({'name': "gazan_average_response_latency", 'timestamp': self.timestamp, 'value': round(lat, 5), 'reaction': reaction})
+                            self.local_vars.append({'name': a[0], 'timestamp': self.timestamp, 'value': a[-1], 'reaction': reaction})
+            # result = histogram_quantile(0.95, buckets)
+            for x in [0.50, 0.75, 0.85, 0.90, 0.95, 0.99]:
+                value = histogram_quantile(x, request)
+                n = "percentile"
+                v = str(int(x*100))
+                self.local_vars.append({'name': "gazan_request_latency", 'timestamp': self.timestamp, 'value': value, 'reaction': reaction, 'check_type': check_type, 'extra_tag': {n: v}})
+            for x in [0.50, 0.75, 0.85, 0.90, 0.95, 0.99]:
+                value = histogram_quantile(x, response)
+                n = "percentile"
+                v = str(int(x*100))
+                self.local_vars.append({'name': "gazan_response_latency", 'timestamp': self.timestamp, 'value': value, 'reaction': reaction, 'check_type': check_type, 'extra_tag': {n: v}})
         except Exception as e:
             lib.puylogger.print_message(__name__ + ' Error : ' + str(e))
             pass
 
 
+def histogram_quantile(q, buckets):
+    sorted_buckets = sorted((float(k), v) for k, v in buckets.items())
+    total = sorted_buckets[-1][1]
+    if total == 0:
+        return None
+    target = q * total
+    prev_le = 0.0
+    prev_count = 0.0
+
+    for le, count in sorted_buckets:
+        if count >= target:
+            delta_count = count - prev_count
+            delta_le = le - prev_le
+            if delta_count == 0:
+                return le
+            return prev_le + ((target - prev_count) / delta_count) * delta_le
+        prev_le = le
+        prev_count = count
+    return sorted_buckets[-1][0]  # fallback (shouldn't hit normally)
